a0dda2ee01228b3de78219dbab826c2e645d00e9
Kyle <Kyle.Flavin@gmail.com>
2019-11-08 16:45:04 -0800

    Add identity support

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,11 +8,15 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { IdentityContextProvider } from 'react-netlify-identity-widget'
+import 'react-netlify-identity-widget/styles.css'
 
 import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+  const url = 'https://kflavin-netlify-identity-functions.netlify.com'
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,22 +29,24 @@ const Layout = ({ children }) => {
 
   return (
     <>
+    <IdentityContextProvider url={url}>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </IdentityContextProvider>
     </>
   )
 }
